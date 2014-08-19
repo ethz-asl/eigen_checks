6d07fc283d2d15ec65df75f147576b881590c932
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-08-19 09:29:11 +0200

    Use std traits to remove reference and some more documentation

--- include/eigen-checks/glog.h
+++ include/eigen-checks/glog.h
@@ -1,15 +1,16 @@
 #ifndef EIGEN_CHECKS_GLOG_H_
 #define EIGEN_CHECKS_GLOG_H_
-#include <glog/logging.h>
 #include <limits>
+#include <type_traits>
+
 #include <eigen-checks/internal/gtest-equal.h>
-#include <eigen-checks/internal/traits.h>
+#include <glog/logging.h>
 
 #define CHECK_EIGEN_MATRIX_EQUAL(MatrixA, MatrixB)                           \
   do {                                                                       \
     ::testing::AssertionResult result = eigen_checks::internal::MatricesNear(\
         MatrixA, #MatrixA, MatrixB, #MatrixB,                                \
-        static_cast<typename eigen_checks::internal::RemoveCR<               \
+        static_cast<typename std::remove_reference<                          \
         decltype(MatrixA)>::type::Scalar>(0.0), "0.0");                      \
     CHECK(static_cast<bool>(result)) << result.message;                      \
   } while(0);
@@ -18,7 +19,7 @@
   do {                                                                       \
     ::testing::AssertionResult result = eigen_checks::internal::MatricesNear(\
         MatrixA, #MatrixA, MatrixB, #MatrixB,                                \
-        static_cast<typename eigen_checks::internal::RemoveCR<               \
+        static_cast<typename std::remove_reference<                          \
         decltype(MatrixA)>::type::Scalar>(                                   \
       eigen_checks::internal::kDefaultPrecision),                            \
       "Floating point precision");                                           \
