d5fc63090191aab1678d90e7c6eef69ce0f0d9c4
timethy <timethy@mailbox.org>
2017-10-24 19:00:38 +0800

    Make entrypoint namespaces global by prefixing with ::

--- include/eigen-checks/entrypoint.h
+++ include/eigen-checks/entrypoint.h
@@ -52,9 +52,9 @@
 
 #define UNITTEST_ENTRYPOINT \
     int main(int argc, char** argv) { \
-  google::InitGoogleLogging(argv[0]); \
-  testing::InitGoogleTest(&argc, argv); \
-  google::ParseCommandLineFlags(&argc, &argv, true); \
+  ::google::InitGoogleLogging(argv[0]); \
+  ::testing::InitGoogleTest(&argc, argv); \
+  ::google::ParseCommandLineFlags(&argc, &argv, true); \
   ::testing::FLAGS_gtest_death_test_style = "threadsafe"; \
   return RUN_ALL_TESTS();\
 }
